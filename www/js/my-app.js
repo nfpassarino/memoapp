3c3d75085a8b28ac69ab759a7fb6f00ddc3f30d2
Nicolás Passarino <nfpassarino@gmail.com>
2021-03-13 20:30:36 -0300

    Agrega crear y mostrar notas
    Agrega crear y mostrar notas por usuario, utilizando firestore.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,26 +15,11 @@ var app = new Framework7({
 
 var mainView = app.views.create('.view-main');
 var router = mainView.router;
-
+var userEmail = '';
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
   $$('#btnIngresar').on('click', loguearUsuario);
   $$('#btnRegistrar').on('click', registrarUsuario);
 })
-
-function loguearUsuario() {
-  const email = $$(emailLog).val();
-  const pass = $$(passLog).val();
-
-  console.log('holaaaa login');
-  firebase.auth().signInWithEmailAndPassword(email, pass)
-  .then((user) => {
-    app.loginScreen.close($$('.login'), true);
-    router.navigate('/perfil/');
-  })
-  .catch((error) => {
-    console.log('Error: ' + error.message + ' [' + error.code + ']');
-  });
-}
 
 function registrarUsuario() {
   const email = $$(emailReg).val();
@@ -47,4 +32,56 @@ function registrarUsuario() {
     .catch((error) => {
       console.log('Error: ' + error.message + ' [' + error.code + ']');
     });
+}
+
+function loguearUsuario() {
+  const email = $$(emailLog).val();
+  const pass = $$(passLog).val();
+  console.log('holaaaa login');
+  firebase.auth().signInWithEmailAndPassword(email, pass)
+  .then((user) => {
+    userEmail = email;
+    app.loginScreen.close($$('.login'), true);
+    router.navigate('/perfil/');
+  })
+  .catch((error) => {
+    console.log('Error: ' + error.message + ' [' + error.code + ']');
+  });
+}
+
+$$(document).on('page:init', '.page[data-name="perfil"]', function (e) {
+  $$('#btnCrearNota').on('click', crearNota);
+  mostrarNotas();
+})
+
+function crearNota() {
+  const titulo = $$(notaTitulo).val();
+  const contenido = $$(notaContenido).val();
+  firebase.firestore().collection('notas').add({
+    notaEmail: userEmail,
+    notaTitulo: titulo,
+    notaContenido: contenido
+  })
+  .then((docRef) => {
+    console.log('Carga exitosa! ');
+    mostrarNotas();
+  })
+  .catch((error) => {
+    console.error("Error writing document: ", error);
+  });
+  //llamar a función que actualice la visualización de notas
+}
+
+function mostrarNotas() {
+  $$('#notasTablero').html('');
+  const queryNotas = firebase.firestore().collection('notas').where('notaEmail', '==', userEmail);
+  queryNotas.get()
+  .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        $$('#notasTablero').append('<div id="' + doc.id + '" class="notas"><h4>' + doc.data().notaTitulo + '</h4><p>' + doc.data().notaContenido + '</p></div>');
+      });
+  })
+  .catch((error) => {
+      console.log("Error getting documents: ", error);
+  });
 }
